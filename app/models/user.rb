8852e9eed9872a95ca8a367c7ef3432e40284587
auracode <vijayendra.iitm@gmail.com>
2017-08-07 23:34:28 +0530

    removed token in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def self.customers
-    @user = User.first
+   @user = User.first
    require 'net/http'
    source = @user.api_source
    resp = Net::HTTP.get_response(URI.parse(source))
    data = resp.body
-   #qA88bHyQ1ydBxhxNsEyK
    @result = JSON.parse(data)
   end
 end
