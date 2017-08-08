6fa92b5be8b96bde4033b98f5ca2de4e44517879
auracode <vijayendra.iitm@gmail.com>
2017-08-08 11:22:43 +0530

    Invoices html fixes and rounded off the amounts

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,27 +1,27 @@
 class Invoice < ApplicationRecord
 	def total
 		@invoice = Invoice.find(self.id)
-		(@invoice.quantity)*(@invoice.rate)
+		((@invoice.quantity)*(@invoice.rate)).round(2)
 	end
 
 	def cgst
 		@invoice = Invoice.find(self.id)
-		(@invoice.total)*0.14
+		((@invoice.total)*0.14).round(2)
 	end
 
 	def sgst
 		@invoice = Invoice.find(self.id)
-		(@invoice.total)*0.14
+		((@invoice.total)*0.14).round(2)
 	end
 
 	def gst
 		@invoice = Invoice.find(self.id)
-		(@invoice.total)*0.28
+		((@invoice.total)*0.28).round(2)
 	end
 
 	def grand_total
 		@invoice = Invoice.find(self.id)
-		(@invoice.total) + (@invoice.gst)
+		((@invoice.total) + (@invoice.gst)).round(2)
 	end
 
 end
